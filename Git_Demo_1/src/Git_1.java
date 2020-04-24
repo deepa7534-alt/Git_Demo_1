dd85c91d7ca9dfd339ec9507633306b306935f9f
Deepanwita <deepanwita2003@gmail.com>
2020-04-24 21:03:35 +0100

    First Commit

---
+++ Git_Demo_1/src/Git_1.java
@@ -0,0 +1,17 @@
+
+public class Git_1 {
+
+	public static void main(String[] args) {
+		
+		String hunger = "yes";
+		
+		if(hunger=="yes") {
+			System.out.println("Eat Something");
+		}
+
+		else {
+			System.out.println("DO NOT EAT");
+		}
+	}
+
+}
